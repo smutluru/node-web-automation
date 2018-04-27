7cff48cf621a73bd98cd343f995f090973e51476
smutluru <36372170+smutluru@users.noreply.github.com>
2018-04-27 15:09:20 +0530

    Modifiying to improve granularity

--- src/command-runner.js
+++ src/command-runner.js
@@ -5,13 +5,20 @@ module.exports = class CommandRunner {
     static exeCMD(cmd, report, values, driver) {
         report.log(cmd._logs ? cmd._logs._start : undefined);
 
+        this.initCMDValues(cmd, values);
+
         switch (cmd.op) {
             case 'use': {
                 this.assertCMD(cmd, ['location']);
                 report.log(`[${cmd._id}] : Using Test Case @ location : ${cmd.location}`);
 
+                let valuesCopy = values;
+                if(cmd._values){
+                    valuesCopy = Object.assign(valuesCopy, cmd._values);
+                }
+
                 let TestRunner = require('./test-runner.js');
-                return TestRunner.executeTC(cmd.location, report, Object.assign({}, values), driver);
+                return TestRunner.executeTC(cmd.location, report, valuesCopy, driver);
             }
 
             case 'call': {
@@ -60,6 +67,20 @@ module.exports = class CommandRunner {
                 return driver.click(cmd.xpath);
             }
 
+            case 'click-js': {
+                this.assertCMD(cmd, ['xpath']);
+                report.log(`[${cmd._id}] : Clicking element @ xpath ${cmd.xpath} via JS`);
+
+                return driver.click_JS(cmd.xpath);
+            }
+
+            case 'double-click' : {
+                this.assertCMD(cmd, ['xpath']);
+                report.log(`[${cmd._id}] : Double Clicking element @ xpath ${cmd.xpath}`);
+
+                return driver.doubleClick(cmd.xpath);
+            }
+
             case 'type': {
                 this.assertCMD(cmd, ['xpath', 'value']);
                 report.log(`[${cmd._id}] : Typing ${cmd.value} @ xpath ${cmd.xpath}`);
@@ -76,6 +97,13 @@ module.exports = class CommandRunner {
                     });
             }
 
+            case 'switch-frame': {
+                this.assertCMD(cmd, ['index']);
+                report.log(`[${cmd._id}] : Switching Frame to ${cmd.index >= 0? ('frame # ' + cmd.index) : 'parent frame'}`);
+
+                return driver.switchFrame(cmd.index >= 0 ? cmd.index : null);
+            }
+
             case 'wait': {
                 this.assertCMD(cmd, ['xpath']);
                 report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}`);
@@ -83,20 +111,39 @@ module.exports = class CommandRunner {
                 return driver.wait(cmd.xpath);
             }
 
+            case 'wait-until-enabled': {
+                this.assertCMD(cmd, ['xpath']);
+                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath} and for it to get enabled.`);
+
+                return driver.wait_until_enabled(cmd.xpath);
+            }
+
             case 'wait-click': {
                 this.assertCMD(cmd, ['xpath']);
-                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}`);
-                report.log(`[${cmd._id}] : Clicking element @ xpath ${cmd.xpath}`);
+                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}, then will click`);
 
-                return driver.wait_click(cmd.xpath);
+                return driver.wait(cmd.xpath).then(_ => driver.click(cmd.xpath));
+            }
+
+            case 'wait-click-js':{
+                this.assertCMD(cmd, ['xpath']);
+                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}, then will click via JS`);
+
+                return driver.wait(cmd.xpath).then(_ => driver.click_JS(cmd.xpath));
+            }
+
+            case 'wait-double-click': {
+                this.assertCMD(cmd, ['xpath']);
+                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}, then will double click`);
+
+                return driver.wait(cmd.xpath).then(_ => driver.doubleClick(cmd.xpath));
             }
 
             case 'wait-type': {
                 this.assertCMD(cmd, ['xpath', 'value']);
-                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}`);
-                report.log(`[${cmd._id}] : Typing ${cmd.value} @ xpath ${cmd.xpath}`);
+                report.log(`[${cmd._id}] : Waiting for element @ xpath ${cmd.xpath}, then will type ${cmd.value}`);
 
-                return driver.wait_type(xpath, value);
+                return driver.wait(cmd.xpath).then(_ => driver.type(cmd.xpath, cmd.value));
             }
 
             default: {
